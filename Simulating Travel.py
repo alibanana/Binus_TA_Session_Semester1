307445a2aa3ca84c0979e7de8beca49fb59d86d2
Alifio Rasyid <official.alifio@gmail.com>
2018-09-18 15:57:20 +0700

    second commit

---
+++ Simulating Travel.py
@@ -0,0 +1,28 @@
+time = int(input ("Time Spent = "))
+acc = int(input ("Acceleration = "))
+dist = int(input ("Distance = "))
+velocity = time * acc
+dist_sec = 0
+s = (1/2)*acc*time*time
+star = 0
+
+for i in range (0, time + 1) :
+    dist_sec = (1 / 2) * acc * i * i
+    star = int(dist_sec//10)
+    print("Duration: ", i,"Distance: ", "*" * star)
+
+#final velocity
+if velocity > 60:
+    print ("The person went over the speed limit.", "(Max speed was",velocity, "m/s)")
+else:
+    print ("The person did not went over the speed limit.", "(Max speed was",velocity, "m/s)")
+
+#final distance
+if s >= dist :
+    print ("The person reached the destination."," (Reached",s,"m)")
+elif s < dist:
+    print ("The person did not reached the destination.", " (Reached", s, "m)")
+
+
+
+
